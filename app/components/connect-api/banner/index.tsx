7b03e5a6b0f40b921cf29e88fe8d85f1504cd9cc
hassantahir767 <hassan.tahir@stixor.com>
2024-08-13 00:56:35 +0500

    other pages

--- app/components/connect-api/banner/index.tsx
+++ app/components/connect-api/banner/index.tsx
@@ -14,15 +14,15 @@ export default function ConnectBanner() {
       <div className="flex justify-center items-center">
         <div className="  bg-blue-blur/50  absolute bottom-[2rem] py-[2.875rem] px-[2.75rem] rounded-[50px] backdrop-blur-[0.5rem] md:w-[90%] block mx-auto">
           <div className="md:flex-row flex-col flex">
-            <div className=" flex-col justify-center items-center gap-[34px] inline-flex md:w-[70%]">
-              <h2 className="self-stretch text-white text-[4.5rem] md:text-[7.375rem] font-medium  md:leading-[8rem] leading-[4.5rem] py-2">
+            <div className=" flex-col justify-center items-center inline-flex md:w-[70%]">
+              <h2 className="self-stretch font-poppinsM  text-white text-[5.5rem]">
                 Travelingo
               </h2>
-              <h2 className="self-stretch text-white text-[4.5rem] md:text-[7.375rem] font-medium  md:leading-[4rem] ">
+              <h2 className="self-stretch font-poppinsM  text-white text-[5.5rem]">
                 Connect API
               </h2>
             </div>
-            <div className=" text-white text-md md:w-[30%] mt-5 md:mt-auto">
+            <div className=" text-white font-poppinsR  text-md md:w-[30%] mt-5 md:mt-auto">
               Integrate comprehensive travel information effortlessly with
               Travelingo Connect API. Access visa details, cultural insights,
               travel budgeting, and language learning resources—all in one
