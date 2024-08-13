ae6290deb1388af3c024bf6a81ae39351d5b7286
hassantahir767 <hassan.tahir@stixor.com>
2024-08-13 22:33:42 +0500

    navbar fixes

--- app/components/shared/navbar/index.tsx
+++ app/components/shared/navbar/index.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="  bg-blue-blur/50  absolute top-0 py-[1.875rem] px-[2.75rem]  backdrop-blur-[0.5rem] w-screen block mx-auto">
+    <div className="  bg-blue-blur/50  absolute top-0 py-[1.3rem] px-[2.75rem]  backdrop-blur-[0.5rem] w-screen block mx-auto">
       <div className="wrapper flex  items-center justify-between">
         <div className="flex flex-row gap-x-10 items-center">
           <Link href={"/"}>
@@ -31,7 +31,7 @@ function Navbar() {
         </div>
 
         <div className="flex gap-x-5">
-          <button className="border-2 border-white flex rounded-[40px] transition-all p-3 items-center hover:bg-light-sky-blue hover:delay-100">
+          <button className="border-2 border-white flex rounded-[40px] transition-all py-2 px-3 items-center hover:bg-light-sky-blue hover:delay-100">
             <Image
               src={"/icons/global.svg"}
               className="mr-2"
@@ -50,9 +50,11 @@ function Navbar() {
 
           <Link
             href={"/connect-api"}
-            className="border-2 border-white flex rounded-[40px] transition-all px-10 py-3 items-center hover:bg-light-sky-blue hover:delay-100"
+            className="border-2 border-white flex rounded-[40px] transition-all px-10 py-2 items-center hover:bg-light-sky-blue hover:delay-100"
           >
-            <p className="font-poppinsR text-white">Traveling API</p>
+            <p className="font-poppinsR text-white text-[1rem]">
+              Traveling API
+            </p>
           </Link>
 
           <Image
