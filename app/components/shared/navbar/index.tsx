fb21abb66f73db353b8f6369af71702b25cc082c
hassantahir767 <hassan.tahir@stixor.com>
2024-08-13 22:30:41 +0500

    navbar

--- app/components/shared/navbar/index.tsx
+++ app/components/shared/navbar/index.tsx
@@ -1,28 +1,67 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="  bg-blue-blur/50  absolute top-0 py-[1.875rem] px-[2.75rem]  backdrop-blur-[0.5rem] w-screen block mx-auto">
       <div className="wrapper flex  items-center justify-between">
         <div className="flex flex-row gap-x-10 items-center">
-          <Image
-            src={"/images/logo.png"}
-            className="w-[10rem] mr-4"
-            width={500}
-            height={500}
-            alt="logo"
-          />
-
-          <p className="font-poppinsR text-white hover:underline cursor-pointer hover:opacity-45">
+          <Link href={"/"}>
+            <Image
+              src={"/images/logo.png"}
+              className="w-[10rem] mr-4 cursor-pointer hover:opacity-40"
+              width={500}
+              height={500}
+              alt="logo"
+            />
+          </Link>
+          <Link
+            href={"/"}
+            className="font-poppinsR text-white hover:underline cursor-pointer hover:opacity-45"
+          >
             Home
-          </p>
+          </Link>
           <p className="font-poppinsR text-white hover:underline cursor-pointer hover:opacity-45">
             Features
           </p>
           <p className="font-poppinsR text-white hover:underline cursor-pointer hover:opacity-45">
             How it Works
           </p>
+        </div>
+
+        <div className="flex gap-x-5">
+          <button className="border-2 border-white flex rounded-[40px] transition-all p-3 items-center hover:bg-light-sky-blue hover:delay-100">
+            <Image
+              src={"/icons/global.svg"}
+              className="mr-2"
+              width={30}
+              height={30}
+              alt=""
+            />
+            <p className="font-poppinsR text-white">EN</p>
+            <Image
+              src={"/icons/arrow-down.svg"}
+              width={30}
+              height={30}
+              alt=""
+            />
+          </button>
+
+          <Link
+            href={"/connect-api"}
+            className="border-2 border-white flex rounded-[40px] transition-all px-10 py-3 items-center hover:bg-light-sky-blue hover:delay-100"
+          >
+            <p className="font-poppinsR text-white">Traveling API</p>
+          </Link>
+
+          <Image
+            className="cursor-pointer"
+            src={"/icons/menu.svg"}
+            width={50}
+            height={50}
+            alt=""
+          />
         </div>
       </div>
     </div>
