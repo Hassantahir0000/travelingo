4e39dab14b821d17532cac51d1337fc43105825e
hassantahir767 <hassan.tahir@stixor.com>
2024-08-14 18:48:55 +0500

    Culture , LocalCustoms

--- app/travel-info/page.tsx
+++ app/travel-info/page.tsx
@@ -4,6 +4,8 @@ import Food from "../../components/travel-info/food";
 import TravelBudget from "../../components/travel-info/travel-budget";
 import TravelInfoCards from "../../components/travel-info/travel-info-cards";
 import VisaInfo from "../../components/travel-info/visa-info";
+import Culture from "@/components/travel-info/culture";
+import LocalCustoms from "@/components/travel-info/local-customs";
 
 export default function TravelInfo() {
   return (
@@ -14,6 +16,8 @@ export default function TravelInfo() {
         <VisaInfo />
         <TravelBudget />
         <Food />
+        <Culture />
+        <LocalCustoms />
         <Footer
           heading={"Travelingo API"}
           description={""}
