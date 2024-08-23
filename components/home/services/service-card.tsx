f648d4aaea968fed4560994f38ab583c204f169a
hassantahir767 <hassan.tahir@stixor.com>
2024-08-24 02:02:04 +0500

    m

--- components/home/services/service-card.tsx
+++ components/home/services/service-card.tsx
@@ -19,7 +19,7 @@ const ServiceCard: FC<CardProps> = ({ data, index, arrayLength }) => {
           router.push("/explore");
         }
       }}
-      className="service-card flex-col relative items-start justify-center w-full above-md:h-screen "
+      className="service-card cursor-pointer flex-col relative items-start justify-center w-full above-md:h-screen "
     >
       <div className="left-0 right-0 my-6  rounded-xl flex-col md:flex-row justify-center lg:my-0 md:mx-auto above-md:absolute">
         <div className="justify-center text-center text-nk-black w-full">
